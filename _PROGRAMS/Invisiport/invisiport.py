8fd028bb9205521bd0d1668ce69eef9feb8da201
kjk <kjkjk1178@gmail.com>
2018-09-26 19:21:18 +0900

    Modify invisiport

--- _PROGRAMS/Invisiport/invisiport.py
+++ _PROGRAMS/Invisiport/invisiport.py
@@ -1,21 +1,26 @@
+# -*- coding: utf-8 -*-
 #!/usr/bin/env python
 import socket
 import os
+import argparse
 
-#hosts to avoid blacklisting
+#블랙리스트에 들어가지 않을 호스트들 
 whitelist = ['127.0.0.1','localhost']
 
-#ports to show as up to blacklisted clients
-#Think about what the attacker will think if he sees different profiles before and after a scan
-ports = ['21','80','445']
+#블랙리스트에 오른 클라이언트에게 열린 것처럼 보여줄 포트들
+#ports = ['21','80','445']
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--triggerport", type=int, help="A scan here will trigger the defenses", required=True)
+parser.add_argument("-f", "--fakeport", type=int, nargs='+', help="ports to show as up to blacklisted clients", required=True)
+parser.add_argument("-a", "--allowport", type=int,  nargs='+', help="ports not to fake")
+parser.add_argument("-r", "--redirectport", type=int, help="port to redirect")
+args = parser.parse_args()
 
 #IP to bind to.  Leave as empty string to bind to all available IPs
 ADDR=''
 
-#Port to bind to.  This will be the listening port.  A scan here will trigger the defenses
-PORT = 443
-PORTSPOOF = 4444
-COWRIE = 2222
 
 #Name of blacklist file
 filename = "blacklist"
@@ -35,17 +40,24 @@ def blacklist(ip):
 	if ip in whitelist or check_blacklist(ip):
 		return False
 	else:
+		
 		#drop except cowrie and portspoof
-		query = "iptables -A INPUT -s %s -p tcp -m multiport ! --destination-port %s,%s -j DROP" % (ip, PORTSPOOF, COWRIE)
+		query = "iptables -A INPUT -s %s -p tcp" % (ip) 
+		if args.allowport :
+			query += " -m multiport ! --destination-port " + str(args.allowport)[1:-1].replace(" ", "")
+		query +=  " -j DROP"
 		os.system(query)
-		for port in ports:
-			query = "iptables -t nat -A PREROUTING -s %s -p tcp --dport %s -j REDIRECT --to-port %s" % (ip,port, PORTSPOOF)
+
+		#config redirect
+		for port in args.fakeport:
+			query = "iptables -t nat -A PREROUTING -s %s -p tcp --dport %s -j REDIRECT --to-port %s" % (ip,port, str(args.redirect))
 			os.system(query)
+
 		add_blacklist(ip)
 	return True
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((ADDR,PORT))
+s.bind((ADDR, args.triggerport))
 s.listen(5)
 
 while True:
